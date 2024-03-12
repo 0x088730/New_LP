b9fab339351837a379d92772842b411bdc7f8921
alexander0923 <alexandermitchell923@gmail.com>
2024-03-12 09:36:52 -0700

    test cdn

--- next.config.js
+++ next.config.js
@@ -19,5 +19,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   ...nextConfig,
-  assetPrefix: isProd ? 'https://cdn.mydomain.com' : undefined,
+  assetPrefix: isProd ? 'https://cdn.mydomain.com' : "",
 }
