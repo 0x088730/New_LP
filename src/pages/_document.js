8128e586ac511478d70816b7d3cc51a76c05c4c0
alexander0923 <alexandermitchell923@gmail.com>
2024-03-14 15:32:18 -0700

    back

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Add viewport meta tag */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          {/* You can add other meta tags, link tags, or script tags here */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
